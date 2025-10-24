71840760eebdf80e81cb6d82fee4f9f66a9e1c8e
Agada Unogwu <unogwuagada@gmail.com>
2025-10-24 08:05:10 -0700

    more updates

--- server/Src/Models/userModel.js
+++ server/Src/Models/userModel.js
@@ -21,9 +21,10 @@ const userSchema = new mongoose.Schema({
     },
 
     avatar: {
-
+        type: String,
+        default: ""
     }
-})
+});
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
@@ -32,23 +33,24 @@ userSchema.pre('save', async function(next) {
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt)
-    }catch(error) {
+        next();
+    } catch(error) {
         next(error)
     }
-})
+});
 
-// Method to compare the passwords
+// Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password)
-}
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
-//Remove password from json output
+// Remove password from JSON output
 userSchema.methods.toJSON = function () {
-    const user = this.Object();
-    delete user.password
+    const user = this.toObject();
+    delete user.password;
     return user;
-}
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-export default User
+export default User;
